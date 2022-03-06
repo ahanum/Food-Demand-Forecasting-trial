910346a72e768e7cd5648f0bc9b0f3803d07c687
ahanum <100678287+ahanum@users.noreply.github.com>
2022-03-06 15:48:44 +0800

    Update eda.py

--- eda.py
+++ eda.py
@@ -5,7 +5,7 @@ import plotly.figure_factory as ff
 import matplotlib.pyplot as plt
 
 
-st.title('Food Demand Forecasting - Analytics Vidhya')
+st.title('Food Demand Forecasting')
 
 @st.cache
 def load_data(nrows):
@@ -33,7 +33,7 @@ st.subheader('Weekly Demand Data')
 st.write(weekly_data)
 
 st.bar_chart(weekly_data['num_orders'])
-df = pd.DataFrame(weekly_data[:200], columns = ['num_orders','checkout_price','base_price'])
+df = pd.DataFrame(weekly_data[:500], columns = ['num_orders','checkout_price','base_price'])
 df.hist()
 
 fig, ax = plt.subplots()
@@ -45,7 +45,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.line_chart(df)
 
-chart_data = pd.DataFrame(weekly_data[:40], columns=['num_orders', 'base_price'])
+chart_data = pd.DataFrame(weekly_data[:50], columns=['num_orders', 'base_price'])
 st.area_chart(chart_data)
 
 #Center Information
@@ -68,30 +68,3 @@ st.bar_chart(meal_data['cuisine'])
 agree = st.button('Click to see Categories of Meal')
 if agree:
     st.bar_chart(meal_data['category'])
-
-
-import streamlit.components.v1 as components
-st.title('Streamlit Components')
-components.html(
-    """
-     
-    <div class="container">
-  <h2>HackerShrine</h2>
- 
-    <div class="card" style="width:400px">
-     
-    <div class="card-body ">
-      <form action="/upload" method="post" enctype="multipart/form-data">
-      <p class="card-text">Custom HTML </p>
-        <input type="file" name="file" value="file">
-        <hr>
-      <input type="submit" name="upload" value="Upload" class="btn btn-success">
-      </form>
-     
-    </div>
-  </div>
-  <br>
-</div>
-    """,
-    height=600,
-)
